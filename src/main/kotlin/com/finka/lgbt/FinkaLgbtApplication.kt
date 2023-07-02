22ca76ab3f99d4bdbc21c5f8924e3f1fd80e25e5
OneCOlyl <sverqw@gmail.com>
2023-07-02 21:25:10 +0800

    init: create master branch

---
+++ src/main/kotlin/com/finka/lgbt/FinkaLgbtApplication.kt
@@ -0,0 +1,11 @@
+package com.finka.lgbt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinkaLgbtApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinkaLgbtApplication>(*args)
+}
---
+++ src/test/kotlin/com/finka/lgbt/FinkaLgbtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.finka.lgbt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinkaLgbtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
